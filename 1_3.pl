9cb567455264003ae6055d005a27dd3bf5250d34
SeraphinaLiang <51225863+SeraphinaLiang@users.noreply.github.com>
2021-10-10 12:46:36 +0200

    Update 1_3.pl

--- 1_3.pl
+++ 1_3.pl
@@ -3,7 +3,7 @@ or(B1,B2) :- B1=tru ; B2=tru.
 not(B) :- B =fal.
 eval(K,M) :- K=tru,M=tru;
              K=fal,M=fal;
-             K=tru,M;
+              % K=tru,M; 多余
              K,M=tru;
-             K=fal,\+M;
+              % K=fal,\+M; 多余
              \+K,M=fal.
