845fe38f5a56ced84a75a813fe5d4aabb9c72e51
SeraphinaLiang <51225863+SeraphinaLiang@users.noreply.github.com>
2021-10-17 17:02:57 +0200

    Add files via upload

---
+++ 2_1.pl
@@ -0,0 +1,19 @@
+link(a,b).
+link(b,a).
+link(b,c).
+link(c,b).
+link(b,d).
+link(d,b).
+link(c,d).
+link(d,c).
+
+/*neighbor*/
+neighbor(X,Y):-link(X,Y);link(Y,X).
+
+/*path which will never end in circles*/
+path(X,Y):-link(X,Y);link(X,T),path(T,Y).
+
+/*path which avoid circles*/
+path2(X,Y):-path2Helper(X,Y,[X]).
+path2Helper(X,Y,V):-link(X,Y),\+(member(Y,V));
+                    link(X,T),\+(member(T,V)),path2Helper(T,Y,[T|V]).
