6cb5a9cdf4da1c5e844f758558738f128c113784
SeraphinaLiang <51225863+SeraphinaLiang@users.noreply.github.com>
2021-10-17 17:35:55 +0200

    Update 1_2.pl

--- 1_2.pl
+++ 1_2.pl
@@ -5,5 +5,5 @@ peano_plus(s(X),Y,s(Z)) :- peano_plus(X,Y,Z).
 min(X,X,zero).
 min(s(X),Y,s(Z)) :- min(X,Y,Z).
 
-greater_than(X,Y) :- min(X,Y,Z),Z \=zero.
+greater_than(X,Y) :- min(X,Y,Z),Z \== zero.
 maximum(X,Y,Z) :- greater_than(X,Y), Z=X; greater_than(Y,X),Z=Y;Z=X.
