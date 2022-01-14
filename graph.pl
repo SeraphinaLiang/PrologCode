62872d571a40cf3972e764b7d6ed725885eb0371
Yinqi Liang <51225863+SeraphinaLiang@users.noreply.github.com>
2022-01-14 10:51:40 +0100

    Update graph.pl

--- graph.pl
+++ graph.pl
@@ -26,4 +26,42 @@ path1(A,[Y|Path1],Cost1,Graph,Path,Cost):-
     Cost2 is Cost1 + CostXY,
     path1(A,[X,Y|Path1],Cost2,Graph,Path,Cost).
     
+% -------------finding a spanning tree of a graph in spread way--------------
+
+stree(Graph,Tree):-
+    member(Edge,Graph),
+    spread([Edge],Tree,Graph).
+
+% spread(Tree1,Tree,Graph) : Tree1 spread to Tree
+spread(Tree1,Tree,Graph):-
+    addEdge(Tree1,Tree2,Graph),
+    spread(Tree2,Tree,Graph).
+spread(Tree,Tree,Graph):-
+    \+addEdge(Tree,_,Graph). % no edge can be add any more, without creating a cycle
+
+% addEdge(Tree,NewTree,Graph)
+addEdge(Tree,[A-B|Tree],Graph):-
+    adjacent(A,B,Graph),
+    node(A,Tree),
+    \+node(B,Tree).
+
+% Graph = [a-b,b-c,b-d,c-d]
+adjacent(N1,N2,Graph):-
+    member(N1-N2,Graph);
+    member(N2-N1,Graph).
+
+node(N,Graph):-
+    adjacent(N,_,Graph). % node is a node in graph, if node is adjacent to anything in graph
+
+% -------------finding a spanning tree of a graph in definition way--------------
+% 1. T is a spanning tree of G if:
+%    - T is a subset of G
+%    - T is a Tree
+%    - each node in G is also in T
+% 2. T is a tree if:
+%    - T is connected
+%    - T has no cycle
+
+
+
 
