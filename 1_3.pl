1de2118168eae40e7af2103cd0b73c387595e4fd
Yinqi Liang <51225863+SeraphinaLiang@users.noreply.github.com>
2023-08-11 13:10:53 +0200

    Update 1_3.pl

--- 1_3.pl
+++ 1_3.pl
@@ -3,7 +3,6 @@ or(B1,B2) :- B1=tru ; B2=tru.
 not(B) :- B =fal.
 eval(K,M) :- K=tru,M=tru;
              K=fal,M=fal;
-              % K=tru,M; 多余
+             % evaluate K to tru/fal
              K,M=tru;
-              % K=fal,\+M; 多余
              \+K,M=fal.
