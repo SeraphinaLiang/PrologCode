58e2610a3217f3bd7e2fe6a3dd2b8b7b1be90a06
Yinqi Liang <51225863+SeraphinaLiang@users.noreply.github.com>
2023-08-15 17:04:11 +0200

    Update more_exercise.pl

--- more_exercise.pl
+++ more_exercise.pl
@@ -1,10 +1,11 @@
 % minmaxtree(node(1-node(2-node(1-leaf(who), 3-leaf(knew)),
 % 4-node(0-leaf(prolog), 1-leaf(was))), 3-node(4-node(5-leaf(such),
 % 2-leaf(fun)), 6-node(2-leaf(!), 4-leaf(!)))), Result).
+% Result = prolog
 
 minmaxtree(T,R):- minmaxtree_min(T,R).
 
-% node(C1-node(T1),C2-node(T2)) ----> WRONG
+% node(C1-node(T1),C2-node(T2)) ----> WRONG, cant unify, !! cannot mix terms and variables !!
 
 minmaxtree_max(leaf(R),R).
 minmaxtree_max(node(C1-T1,C2-T2),R):-
@@ -17,3 +18,5 @@ minmaxtree_min(node(C1-T1,C2-T2),R):-
     C1 >= C2,
     minmaxtree_max(T2,R);
     minmaxtree_max(T1,R).
+
+-------------------------------------------------------------------
